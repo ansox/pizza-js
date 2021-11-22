36f65f88bebc6e95712bb187367b02e38626c5a0
Anso <anderson.floriano@gmail.com>
2021-11-21 22:56:35 -0300

    Add battle events

---
+++ Battle/BattleEvent.js
@@ -0,0 +1,33 @@
+class BattleEvent {
+  constructor(event, battle) {
+    this.event = event;
+    this.battle = battle;
+  }
+
+  textMessage(resolve) {
+    const message = new TextMessage({
+      text: this.event.text,
+      onComplete: () => {
+        resolve();
+      }
+    })
+
+    message.init(this.battle.element);
+  }
+
+  submissionMenu(resolve) {
+    const menu = new SubmissionMenu({
+      caster: this.event.caster,
+      enemy: this.event.enemy,
+      onComplete: submission => {
+        resolve(submission);
+      }
+    })
+
+    menu.init(this.battle.element);
+  }
+
+  init(resolve) {
+    this[this.event.type](resolve);
+  }
+}
---
+++ Content/actions.js
@@ -0,0 +1,8 @@
+window.Actions = {
+  damage1: {
+    name: 'Whomp!',
+    success: [
+      { type: 'textMessage', text: '{CASTER} uses Whomp!'}
+    ]
+  }
+}
